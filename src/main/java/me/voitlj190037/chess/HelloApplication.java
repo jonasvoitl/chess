1088128944eb256f4f6a55aefbedceb6d7aab067
Jonas Voitl <VoitlJ190037@sus.htl-grieskirchen.at>
2022-01-17 09:36:05 +0100

    test merge

--- src/main/java/me/voitlj190037/chess/HelloApplication.java
+++ src/main/java/me/voitlj190037/chess/HelloApplication.java
@@ -15,6 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("FNAF!");
         stage.setScene(scene);
         stage.show();
+        //test
     }
 
     public static void main(String[] args) {
