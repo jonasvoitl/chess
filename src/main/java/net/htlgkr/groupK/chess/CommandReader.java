bde11e13592fc41aeda899bf5070c428a5099fb9
Michael-Van-Robbe <mittermayrm180114@sus.htl-grieskirchen.at>
2022-02-01 12:20:10 +0100

    test commit for test merge

--- src/main/java/net/htlgkr/groupK/chess/CommandReader.java
+++ src/main/java/net/htlgkr/groupK/chess/CommandReader.java
@@ -1,6 +1,7 @@
 package net.htlgkr.groupK.chess;
 
 public class CommandReader {
+    //Test comment for merge
     private final String CLIENT_ABBREVIATION = "[C]";
     private final String SERVER_ABBREVIATION = "[S]";
 }
