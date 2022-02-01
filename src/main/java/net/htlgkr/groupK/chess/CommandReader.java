c90e2111c255d58471b65395c0febb19b52c9a4c
Jonas Voitl <VoitlJ190037@sus.htl-grieskirchen.at>
2022-02-01 12:24:22 +0100

    test commit for test merge

--- src/main/java/net/htlgkr/groupK/chess/CommandReader.java
+++ src/main/java/net/htlgkr/groupK/chess/CommandReader.java
@@ -3,4 +3,5 @@ package net.htlgkr.groupK.chess;
 public class CommandReader {
     private final String CLIENT_ABBREVIATION = "[C]";
     private final String SERVER_ABBREVIATION = "[S]";
+    //Second Test comment for test merge
 }
